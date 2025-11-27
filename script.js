dd57e812f76cc62cc3f2ac1425de09386bb96b70
kocheng-bharbar <53999929+kocheng-bharbar@users.noreply.github.com>
2025-11-27 16:59:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const nameField = document.getElementById("name");
 const idnumField = document.getElementById("idnum");
 const infoField = document.getElementById("info");
 const photoField = document.getElementById("photo");
-const errorField = document.getElementById("error");
+const errorField = document.getElementById("errorText");
 
-if (!id) {
+if(!id){
     errorField.textContent = "ID tidak ditemukan di URL.";
-} else {
+    errorField.parentElement.style.display = "flex";
+}else{
     fetch(`data/${id}.json`)
-        .then(res => {
-            if (!res.ok) throw new Error("Data tidak ditemukan.");
+        .then(res=>{
+            if(!res.ok) throw new Error("Data tidak ditemukan");
             return res.json();
         })
-        .then(data => {
+        .then(data=>{
             nameField.textContent = data.name;
             idnumField.textContent = data.idnum;
             infoField.textContent = data.info;
-            photoField.src = data.photo; // otomatis ke foto/abc.jpg
+            photoField.src = data.photo;
+            if(!data.photo || data.photo.trim()===""){
+                photoField.style.display="none";
+                photoField.nextElementSibling.style.display="grid";
+            }
         })
-        .catch(err => {
+        .catch(err=>{
             errorField.textContent = err.message;
+            errorField.parentElement.style.display = "flex";
         });
 }
